30c0f2e1d967f92c00b5edcb1597088d0e9f5a4b
mart10vse <mart10@vse.cz>
2021-03-18 13:57:54 +0100

    initial commit

---
+++ settings/index.jsx
@@ -0,0 +1,13 @@
+registerSettingsPage(({ settings }) => (
+  <Page>
+    <Section
+      title={
+        <Text bold align="center">
+          App Settings
+        </Text>
+      }
+    >
+      <Text>Ahoj!</Text>
+    </Section>
+  </Page>
+));
